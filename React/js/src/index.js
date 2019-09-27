d2861a0e613d8ef3a5c45ea24994fc96c188dac0
Yi Zhang <1711646373@qq.com>
2019-09-27 16:28:14 +0800

    2019年9月27日16点28分

---
+++ React/js/src/index.js
@@ -0,0 +1,6 @@
+import A from "./mod"
+
+let a = new A(100)
+a.show();
+
+foo();
---
+++ React/js/src/mod.js
@@ -0,0 +1,13 @@
+//缺省导出
+export default class A{
+    constructor(x){
+        this.x = x;
+    }
+    show() {
+        console.log(this.x)
+    }
+}
+
+export function foo() {
+    console.log('foo function');
+}
